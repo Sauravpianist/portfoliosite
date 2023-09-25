32380d69f44f14b7d5d5f269bd11ac2a72fae3bd
Saurav Pianist <sauravpunworld2@gmail.com>
2023-09-25 21:02:11 +0530

    update #1

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,26 @@
 import React from 'react';
+//images
+// import Logo from '../assets/logo.svg';
+
 
 const Header = () => {
-  return <div>Header</div>;
+  return <header className='py-8' >
+  <div className="container mx-auto">
+    <div className='flex justify-between items-center'>
+      {/*logo*/}
+      <a href="#" className='text-gradient btn-link'>
+        Saurav Pianist
+        {/* <img src={Logo} alt="" /> */}
+      </a>
+      {/*button*/}
+      <button className='btn btn-sm'>work with me</button>
+
+
+    </div>
+  </div>
+  
+  </header>;
+
 };
 
 export default Header;
